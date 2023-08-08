cc3402b0a6d7bfb3df9328b102ccbdda7bb1daa1
kou1046 <iwashiro0517@yahoo.co.jp>
2023-08-08 23:29:04 +0900

    api通信するサンプル処理の追加

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,9 @@
-console.log("Hello world from background!");
+chrome.runtime.onMessage.addListener(async (request) => {
+  const res = await fetch(process.env.HOST as string, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(request),
+  });
+});
--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,10 @@
-alert("Hello world from content script!");
+document.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  if (!body) {
+    return;
+  }
+
+  chrome.runtime.sendMessage({ body: body.outerHTML }, (res_from_background) =>
+    console.log(res_from_background)
+  );
+});
