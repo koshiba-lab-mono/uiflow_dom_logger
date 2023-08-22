0bf522e5d44e5340d3885874ea5cbd8ed3e99580
kou1046 <iwashiro0517@yahoo.co.jp>
2023-08-22 19:28:29 +0900

    サーバー側のファイルや処理をすべて消去

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,5 @@
 import { ContentStore, ContentType } from "./stores/contentStore";
 import { IndexedDBStore } from "./stores/indexedDBStore";
-import { PostBackEndStore } from "./stores/postBackEndStore";
 
 const main = async () => {
   const store = await IndexedDBStore.createInstance();
