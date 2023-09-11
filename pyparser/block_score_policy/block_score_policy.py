d22e35dd2db7470c7b3e6ef671c0bb10a9fcdb7f
kou1046 <iwashiro0517@yahoo.co.jp>
2023-09-11 16:29:56 +0900

    [Add]: BlockScorePolicy#each_score

--- pyparser/block_score_policy/block_score_policy.py
+++ pyparser/block_score_policy/block_score_policy.py
@@ -11,9 +11,15 @@ class BlockScorePolicy(metaclass=ABCMeta):
     def add(self, rule: BlockScoreRule):
         self.rules.append(rule)
 
-    def comply_with_all(self, blocks_collection: list[list[Block]]):
+    def comply_with_all(self, blocks_collection: list[list[Block]]) -> int:
         score = 0
         for rule in self.rules:
             score += rule.score(blocks_collection)
 
         return score
+
+    def each_score(self, blocks_collection: list[list[Block]]) -> list[tuple[str, int]]:
+        name_scores: list[tuple[str, int]] = []
+        for rule in self.rules:
+            name_scores.append((rule.__class__.__name__, rule.score(blocks_collection)))
+        return name_scores
