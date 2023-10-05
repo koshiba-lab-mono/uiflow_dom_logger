bfb9439149c3abbf47b29d8f6b9d944b1612cffd
kou1046 <iwashiro0517@yahoo.co.jp>
2023-10-05 16:06:50 +0900

    [Change]: score_policyの各メソッド引数を変更

--- pyparser/block_score_policy/block_score_policy.py
+++ pyparser/block_score_policy/block_score_policy.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod, ABCMeta
 
+
 from ..block_score_rules.block_score_rule import BlockScoreRule
 from ..blocks.block import Block
+from ..blocks.uiflow_block_dom import UiflowBlockDOM
 
 
 class BlockScorePolicy(metaclass=ABCMeta):
@@ -11,18 +13,18 @@ class BlockScorePolicy(metaclass=ABCMeta):
     def add(self, rule: BlockScoreRule):
         self.rules.append(rule)
 
-    def comply_with_all(self, blocks_collection: list[list[Block]]) -> int:
+    def comply_with_all(self, uiflow_block_dom: UiflowBlockDOM) -> int:
         score = 0
         for rule in self.rules:
-            score += rule.score(blocks_collection)
+            score += rule.score(uiflow_block_dom.blocks())
 
         return score
 
     def rule_names(self) -> list[str]:
         return [rule.__class__.__name__ for rule in self.rules]
 
-    def each_score(self, blocks_collection: list[list[Block]]) -> list[tuple[str, int]]:
+    def each_score(self, uiflow_block_dom: UiflowBlockDOM) -> list[tuple[str, int]]:
         name_scores: list[tuple[str, int]] = []
         for rule in self.rules:
-            name_scores.append((rule.__class__.__name__, rule.score(blocks_collection)))
+            name_scores.append((rule.__class__.__name__, rule.score(uiflow_block_dom.blocks())))
         return name_scores
